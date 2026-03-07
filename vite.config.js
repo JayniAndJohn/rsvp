918d22cf4bbf15896bca8e9d76af9c2016856d13
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2026-03-07 17:48:37 +0800

    🔧 Set site path to rsvp on production branch

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rsvp-site/',
+  base: '/rsvp/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
